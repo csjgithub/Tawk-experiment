08d5f744bc09decd5fe6ce8175fb7c99d69cde7b
csjgithub <carsj314@student.liu.se>
2017-04-23 18:04:30 +0200

    Added facebook login functionality

--- app/src/main/java/com/tawk/carl/myapplication/MyProfile.java
+++ app/src/main/java/com/tawk/carl/myapplication/MyProfile.java
@@ -2,10 +2,15 @@ package com.tawk.carl.myapplication;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.facebook.Profile;
+import com.squareup.picasso.Picasso;
 
 public class MyProfile extends AppCompatActivity {
 
@@ -13,15 +18,17 @@ public class MyProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
-        SharedPreferences preferences = getSharedPreferences(
-                "1111", Context.MODE_PRIVATE);
-        String profileName = preferences.getString(SplashActivity.PROFILE_NAME, null);
-        if (profileName == null){
+     SharedPreferences  preferences = this.getSharedPreferences(SplashActivity.PREF_FILE, Context.MODE_PRIVATE);
+
+        if (preferences.getString(SplashActivity.PROFILE_ID,null) == null){
+            Log.d("MyProfiletag", "Profile is null");
             finish();
         }else{
             TextView profileNameView = (TextView)findViewById(R.id.profile_name);
-            profileNameView.setText(profileName);
-
+            profileNameView.setText(preferences.getString(SplashActivity.PROFILE_NAME, ""));
+            ImageView profileImgView = (ImageView) findViewById(R.id.profile_picture);
+            Picasso.with(this).load("http:// graph.facebook.com/" + preferences.getString(SplashActivity.PROFILE_ID, "") + "/picture?" +
+                    "width=100&height=100").into(profileImgView);
         }
 
     }
--- app/src/main/java/com/tawk/carl/myapplication/SplashActivity.java
+++ app/src/main/java/com/tawk/carl/myapplication/SplashActivity.java
@@ -3,47 +3,161 @@ package com.tawk.carl.myapplication;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.preference.Preference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
+
 public class SplashActivity extends AppCompatActivity {
 
-    public static final String PROFILE_NAME = "profileName";
-    private Button buttonname;
-    private EditText editText;
-    private SharedPreferences preferences;
-
-
+    public static final String PREF_FILE = "1111";
+    public static final String TAG = "Hello";
+    public static final String PROFILE_NAME = "Profile Name";
+    public static final String PROFILE_ID = "Profile ID";
+    CallbackManager callbackManager;
+    private LoginButton loginButton;
+    private AccessTokenTracker mAccessTokenTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonname = (Button) findViewById(R.id.tawklogin);
-        editText = (EditText) findViewById(R.id.email);
-        preferences = getSharedPreferences(
-                "1111", Context.MODE_PRIVATE);
-        buttonname.setOnClickListener(new View.OnClickListener() {
+        if (Profile.getCurrentProfile() != null){
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+        final SharedPreferences preference = this.getSharedPreferences(PROFILE_NAME,Context.MODE_PRIVATE);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+        loginButton.setReadPermissions("public_profile");
+
+        // Other app specific specialization
+        callbackManager = CallbackManager.Factory.create();
+        // Callback registration
+        loginToMyFbApp();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+            private ProfileTracker mProfileTracker;
+
             @Override
-            public void onClick(View v) {
+            public void onSuccess(LoginResult loginResult) {
+                if(Profile.getCurrentProfile() == null) {
+                    Log.v(TAG, "Profile is null");
+                    mProfileTracker = new ProfileTracker() {
+                        @Override
+                        protected void onCurrentProfileChanged(Profile profile, Profile profile2) {
+                            // profile2 is the new profile
+                            Log.v(TAG, profile2.getFirstName());
+                            mProfileTracker.stopTracking();
+                            startMain();
+                        }
 
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                if (editText.getText().length() < 5) {
-                    Toast.makeText(SplashActivity.this, "You have too short a name, dummy!!!!",
-                            Toast.LENGTH_LONG).show();
-                } else {
-                    preferences.edit().putString(PROFILE_NAME,editText.getText().toString())
-                            .apply();
-                    startActivity(intent);
-
+                    };
+                    mProfileTracker.startTracking();
+                    // no need to call startTracking() on mProfileTracker
+                    // because it is called by its constructor, internally.
+                }
+                else {
+                    Log.v(TAG, "Profile is NOT null");
+                    startMain();
                 }
             }
 
+            private void startMain() {
+                SharedPreferences.Editor editor = preference.edit();
+                editor.putString(PROFILE_NAME, Profile.getCurrentProfile().getFirstName());
+                //editor.putString("E-mail"), Profile.getCurrentProfile().;
+                editor.putString(PROFILE_ID, Profile.getCurrentProfile().getId());
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onCancel() {
+                Log.v(TAG, "cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                Log.v(TAG, e.getMessage());
+            }
+
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    private void loginToMyFbApp() {
+        if (AccessToken.getCurrentAccessToken() != null) {
+            mAccessTokenTracker = new AccessTokenTracker() {
+                @Override
+                protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                    mAccessTokenTracker.stopTracking();
+                    if(currentAccessToken == null) {
+                        //(the user has revoked your permissions -
+                        //by going to his settings and deleted your app)
+                        //do the simple login to FaceBook
+                        //case 1
+                        Log.v(TAG, "currentAccessToken == null");
+                    }
+                    else {
+                        //you've got the new access token now.
+                        //AccessToken.getToken() could be same for both
+                        //parameters but you should only use "currentAccessToken"
+                        //case 2
+                        Log.v(TAG, "currentAccessToken != null");
+                        fetchProfile();
+                    }
+                }
+            };
+            mAccessTokenTracker.startTracking();
+            AccessToken.refreshCurrentAccessTokenAsync();
+        }
+        else {
+            //do the simple login to FaceBook
+            //case 1
+        }
+    }
+
+    private void fetchProfile() {
+        GraphRequest request = GraphRequest.newMeRequest(
+                AccessToken.getCurrentAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        // this is where you should have the profile
+                        Log.v("fetched info", object.toString());
+                    }
+                });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,link"); //write the fields you need
+        request.setParameters(parameters);
+        request.executeAsync();
     }
 }
